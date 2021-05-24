41fce7a2f67cba2b8ec15186bd5a68cc17e53038
Scott Stringer <scott_ts@hotmail.com>
2021-05-24 13:23:23 -0500

    finished formatting on checkout page

--- YeOldeFake5eShop/Startup.cs
+++ YeOldeFake5eShop/Startup.cs
@@ -61,7 +61,7 @@ namespace YeOldeFake5eShop
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
-            });
+        });
         }
     }
 }
